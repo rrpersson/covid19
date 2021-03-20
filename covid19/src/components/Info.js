f8113e730c2c058be1cad7fb40381563e594eeb4
Rasmus Persson <rrasmuspersson@gmail.com>
2021-03-20 20:47:04 +0100

    Added a simple info screen

--- covid19/src/components/Info.js
+++ covid19/src/components/Info.js
@@ -1,7 +1,23 @@
 import React from "react";
 
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
 const Info = () => {
-  return <div>Info will go here</div>;
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <Typography variant="h6" color="initial">
+          Tack för att du använder denna app!
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" color="initial">
+          Tacks till SkånesIT som uttvecklat denna app.
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Info;
