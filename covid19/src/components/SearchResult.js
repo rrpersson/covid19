7a4fc366075785b2959cdd9fb1961f7ae552c0aa
Rasmus Persson <rrasmuspersson@gmail.com>
2021-03-21 11:45:46 +0100

    Struggling with state

--- covid19/src/components/SearchResult.js
+++ covid19/src/components/SearchResult.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { useState } from "react";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
 
 const SearchResult = () => {
-  const [searchResultCounter, setSearchResultCounter] = useState(0);
-  return (
-    <div>
-      {searchResultCounter}
+  const [searchResult, setSearchResult] = useState("start");
+  const [text, setText] = useState("");
 
-      <Button variant="contained" color="primary">
-        +1
-      </Button>
-    </div>
-  );
+  const setTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  if (searchResult === "start") {
+    return (
+      <Grid container spacing={0}>
+        <Grid item xs={12}>
+          <Grid container justify="center">
+            <TextField
+              variant="filled"
+              id="search"
+              label="Sök på län"
+              placeholder="Skåne"
+              autoFocus="true"
+              onChange={setTextChange}
+              value={text}
+            />
+            <Button variant="contained" color="primary" type="submit">
+              Sök
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  } else {
+    return <div>daljfhksdjfh</div>;
+  }
 };
 
 export default SearchResult;
